64cc5950082c12613f6ed09cc43f36e331ee0893
raffaelecapaldo <raffaelecpl@gmail.com>
2023-10-12 15:59:38 +0200

    Added create and store for Ingredient

--- src/main/java/org/java/app/business/controller/IngredientController.java
+++ src/main/java/org/java/app/business/controller/IngredientController.java
@@ -1,15 +1,16 @@
 package org.java.app.business.controller;
 
-import java.util.List;
 
-import org.java.app.business.db.pojo.Pizza;
+import org.java.app.business.db.pojo.Ingredient;
 import org.java.app.business.db.serv.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("ingredients")
@@ -23,6 +24,28 @@ public class IngredientController {
 		model.addAttribute("ingredients", ingredientService.findAll());
 		
 		return "ingredient/ingredient-index";
+	}
+	
+	@GetMapping("/create")
+	public String getCreateForm(Model model) {
+		
+		model.addAttribute("ingredient", new Ingredient());
+		
+		return "ingredient/ingredient-create";
+	}
+	
+	@PostMapping("/create")
+	public String storeIngredient(Ingredient ingredient, BindingResult bindingResult, Model model, 
+			RedirectAttributes ra) {
+		if (bindingResult.hasErrors()) {
+			 return "ingredient/ingredient-create";
+		}
+		else {
+			ingredientService.save(ingredient);
+			ra.addFlashAttribute("addedMessage", "Ingrediente aggiunto correttamente");
+			return "redirect:/ingredients";
+		}
+		
 	}
 	
 }
